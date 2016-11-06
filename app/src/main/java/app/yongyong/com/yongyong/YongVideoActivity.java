2007d324e17aeecf7b54b9226de1fab26befd909
JustLoveYang <zhangyj251211@gmail.com>
2016-11-06 12:51:27 +0800

    First commit.

---
+++ app/src/main/java/app/yongyong/com/yongyong/YongVideoActivity.java
@@ -0,0 +1,13 @@
+package app.yongyong.com.yongyong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YongVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yong_video);
+    }
+}
